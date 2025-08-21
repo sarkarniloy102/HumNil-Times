001811879aba414d4f66134b7caafc8f625b571a
sarkar niloy <sarkarniloy102@gmail.com>
2025-08-21 18:05:10 +0600

    modified styling in page list

--- src/Components/Pages/Blogs/Pagination.jsx
+++ src/Components/Pages/Blogs/Pagination.jsx
@@ -1,3 +1,5 @@
+import { FaAnglesLeft, FaAnglesRight } from "react-icons/fa6";
+
 const Pagination = ({ onPageChange, blogs, currentPage, BlogPerPage }) => {
 
     const totalPages = Math.ceil(blogs.length / BlogPerPage);
@@ -6,9 +8,9 @@ const Pagination = ({ onPageChange, blogs, currentPage, BlogPerPage }) => {
         return Array.from({ length: totalPages }, (_, i) => i + 1).map((pageNumber) => (
             <li
                 key={pageNumber}
-                className={pageNumber === currentPage ? "bg-orange-500 " : ""} >
+                className={pageNumber === currentPage ? "bg-orange-500 rounded-md " : ""} >
                 <a href="#"
-                className="inline-block border border-zinc-400 px-5 py-2 hover:bg-zinc-400"
+                    className="inline-block border border-zinc-400 px-3 py-1 rounded-md hover:bg-zinc-400"
                     onClick={() => onPageChange(pageNumber)}>{pageNumber}</a>
             </li >
         ))
@@ -16,18 +18,21 @@ const Pagination = ({ onPageChange, blogs, currentPage, BlogPerPage }) => {
     return (
         <div>
             <ul className="flex justify-center items-center list-none m-0 p-0 my-8 flex-wrap gap4">
-                <li className="border border-zinc-400 px-5 py-2 cursor-pointer hover:bg-zinc-400">
+                <li className=" cursor-pointer flex justify-center items-center gap-1 mr-3 hover:font-semibold">
+                    <FaAnglesLeft />
                     <button
                         onClick={() => onPageChange(currentPage - 1)}
                         disabled={currentPage === 1}
                         className="cursor-pointer">Previous</button>
                 </li>
-                <div className="flex gap-0 justify-center items-center">{renderPaginationsLinks()}</div>
-                <li className="border border-zinc-400 px-5 py-2  cursor-pointer hover:bg-zinc-400">
+                <div className="flex gap-2 justify-center items-center">{renderPaginationsLinks()}</div>
+                <li className="  cursor-pointer flex justify-center items-center gap-1 ml-3 hover:font-semibold">
+
                     <button
                         onClick={() => onPageChange(currentPage + 1)}
                         disabled={currentPage === totalPages}
                         className="cursor-pointer">Next</button>
+                    <FaAnglesRight />
                 </li>
             </ul>
         </div>
