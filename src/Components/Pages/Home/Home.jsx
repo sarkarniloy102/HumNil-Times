41aab21433da174040372ad04466b7fa97be5f8a
sarkar niloy <sarkarniloy102@gmail.com>
2025-08-23 00:04:19 +0600

    rendering blogpage in Home page

--- src/Components/Pages/Home/Home.jsx
+++ src/Components/Pages/Home/Home.jsx
@@ -1,11 +1,14 @@
 
+import BlogPage from "../Blogs/BlogPage";
 import Banner from "./Banner";
 
 const Home = () => {
     return (
         <div>
-           
             <Banner />
+            <div className="max-w-7xl mx-auto">
+                <BlogPage/>
+            </div>
         </div>
     );
 };
